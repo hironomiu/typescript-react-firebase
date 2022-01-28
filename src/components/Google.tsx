736d38b52ab42f286c5780784a918848efd97b07
hironomiu <hironomiu@gmail.com>
2022-01-28 17:22:54 +0900

    commited

--- src/components/Google.tsx
+++ src/components/Google.tsx
@@ -2,19 +2,18 @@ import React, { Dispatch, SetStateAction, memo } from 'react'
 import { googleAuthProvider } from '../firebase/auth/googleAuthProvider'
 import { socialMediaAuth } from '../firebase/auth/socialMediaAuth'
 
-const Google: React.FC<{ setIsLogin: Dispatch<SetStateAction<boolean>> }> = ({
-  setIsLogin,
-}) => {
-  const login = () => {
-    socialMediaAuth(googleAuthProvider)
-    setIsLogin(true)
-  }
-  return (
-    <div id="google-auth">
-      <span>Google Auth</span>
-      <button onClick={() => login()}>Google</button>
-    </div>
-  )
-}
+const Google: React.FC<{ setIsLogin: Dispatch<SetStateAction<boolean>> }> =
+  memo(({ setIsLogin }) => {
+    const login = () => {
+      socialMediaAuth(googleAuthProvider)
+      setIsLogin(true)
+    }
+    return (
+      <div id="google-auth">
+        <span>Google Auth</span>
+        <button onClick={() => login()}>Google</button>
+      </div>
+    )
+  })
 
 export default Google
