12176243d1bb2a6cc2e3de53c6767f893cb0cc9d
hironomiu <hironomiu@gmail.com>
2022-03-01 10:35:17 +0900

    commited

--- src/components/Twitter.tsx
+++ src/components/Twitter.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, memo } from 'react'
+import { Dispatch, SetStateAction, memo, useCallback } from 'react'
 import { TwitterAuthProvider } from 'firebase/auth'
 import { twitterAuthProvider } from '../firebase/auth/twitterAuthProvider'
 import { socialMediaAuth } from '../firebase/auth/socialMediaAuth'
@@ -9,16 +9,20 @@ const Twitter: React.FC<{
   user: User
   setUser: React.Dispatch<React.SetStateAction<User>>
 }> = memo(({ setIsLogin, user, setUser }) => {
-  const handleOnClick = async (provider: TwitterAuthProvider) => {
-    const res = await socialMediaAuth(provider)
-    console.log(res)
-    if (res.displayName) {
+  const handleOnClick = useCallback(
+    async (provider: TwitterAuthProvider) => {
+      const res = await socialMediaAuth(provider)
       console.log(res)
-      const email = res.email ? res.email : ''
-      setUser({ ...user, nickname: res.displayName, email: email })
-      setIsLogin(true)
-    }
-  }
+      if (res.displayName) {
+        console.log(res)
+        const email = res.email ? res.email : ''
+        setUser({ ...user, nickname: res.displayName, email: email })
+        setIsLogin(true)
+      }
+    },
+    [user, setUser, setIsLogin]
+  )
+
   return (
     <div id="twitter-auth">
       <span>Twitter Auth</span>
