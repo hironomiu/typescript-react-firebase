c1cf6f35a727386c2bafefab362f18bd1f0a80e9
hironomiu <hironomiu@gmail.com>
2022-07-05 19:50:02 +0900

    commited

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -20,7 +20,7 @@ const Auth = () => {
     <main className="flex flex-col justify-center items-center mt-8">
       <EmailPassword />
       <div className="flex flex-col border-[1px] items-center mt-14 w-96 h-32 rounded">
-        <h1 className="my-2">Sosial SigIn & SignUp</h1>
+        <h1 className="my-2">Social SignIn & SignUp</h1>
         <div className="flex my-2">
           <Twitter />
           <GitHub />
